f5091faab95080a5433cdc41273f8368bf8e576d
20171105113 <s20171105113@FanydeiMac13.local>
2018-06-21 09:32:05 +0800

    1
    first

--- 1/main.cpp
+++ 1/main.cpp
@@ -5,11 +5,12 @@
 //  Created by 李向宏 on 18/6/21.
 //  Copyright © 2018年 李向宏. All rights reserved.
 //
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    char a[2];
+    cin >> a;
+    cout << a <<endl;
     return 0;
 }
