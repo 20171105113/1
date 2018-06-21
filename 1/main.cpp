eabe09289fc72f2974156d75d6eacd5e159b0564
李向宏 <s20171105113@fanydeimac13.local>
2018-06-21 08:38:34 +0800

    Initial Commit

---
+++ 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1
+//
+//  Created by 李向宏 on 18/6/21.
+//  Copyright © 2018年 李向宏. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
